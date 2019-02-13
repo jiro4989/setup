42af1c40610a59259201dceb442dd5c438a8b1e5
jiro4989 <jiroron666@gmail.com>
2019-02-13 23:08:53 +0900

    :tada:init commit

---
+++ dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible-2.7
+sudo apt-get install ansible -y
+sudo apt-get upgrade
