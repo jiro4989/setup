353fcd33a753d7558164256112c51fe7e01c067a
jiro4989 <jiroron666@gmail.com>
2019-03-24 08:28:39 +0900

    :sparkles: add manjaro linux yaml

---
+++ dep_manjaro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+# https://z1000s.hatenablog.com/entry/2017/11/16/223557
+# saitekika
+sudo pacman-mirrors -f 0
+
+# https://qiita.com/MoriokaReimen/items/dbe1448ce6c0f80a6ac1
+# sync repository
+sudo pacman -Syy
+# search software
+pacman -Ss ansible
+# install
+pacman -S --noconfirm ansible
