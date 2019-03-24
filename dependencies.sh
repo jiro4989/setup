7529c4526865eae37c5f4bf615e31b91875fe47b
jiro4989 <jiroron666@gmail.com>
2019-03-24 11:01:53 +0900

    :sparkles: LinuxMint環境で実行したら色々失敗したので修正

--- dependencies.sh
+++ dependencies.sh
@@ -4,6 +4,6 @@ set -eu
 
 sudo apt-get update
 sudo apt-get install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible-2.7
+sudo add-apt-repository --yes ppa:ansible/ansible-2.7
 sudo apt-get install ansible -y
 sudo apt-get upgrade
