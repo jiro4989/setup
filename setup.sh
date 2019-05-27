ff8a51b92ec3fea2a5438ce03c871a47c8b53226
jiro4989 <jiroron666@gmail.com>
2019-05-27 12:56:55 +0900

    :bug: bashの構文エラーを修正

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ if [ "$1" = p ] || [ "$1" = playbook ]; then
 fi
 
 if [ "$USE_PLAYBOOK_ONLY" = false ]; then
-  ./deps.sh || { echo Failed deps.sh; exit 1 }
+  ./deps.sh || { echo Failed deps.sh; exit 1; }
 fi
 
 if [ "$(grep "ID=" /etc/os-release | cut -d "=" -f 2)" = manjaro ]; then
-  ansible-playbook site_manjarolinux.yaml -K || { echo Failed playbook; exit 1 }
+  ansible-playbook site_manjarolinux.yaml -K || { echo Failed playbook; exit 1; }
 else
-  ansible-playbook site_ubuntu.yaml -K || { echo Failed playbook; exit 1 }
+  ansible-playbook site_ubuntu.yaml -K || { echo Failed playbook; exit 1; }
 fi
 
